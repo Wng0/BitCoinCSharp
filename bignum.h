a00ef8f81c39389e3d496059d0c5eebf4d18f15a
Wng0 <Wng0@sina.com>
2020-04-06 22:34:41 +0800

    一天写完了bignum.h 的400多行代码,牛!

--- bignum.h
+++ bignum.h
@@ -220,4 +220,252 @@ class CBigNum:public BIGNUM
 			reverse(vch.begin(),vch.end());
 			return vch;
 		}
+		CBigNum& SetCompact(unsigned int nCompact)
+		{
+			unsigned int nSize =nCompact>>24;
+			std::vector<unsigned char> vch(4+nSize);
+			vch[3]=nSize;
+			if (nSize>=1) vch[4]=(nCompact>>16) &0xff;
+			if (nSize>=2) vch[5]=(nCompact>>8) & 0xff;
+			if (nSize>=3) vch[6]=(nCompact>>0) &0xff;
+			BN_mpi2bn(&vch[0],vch.size(),this);
+			return *this;
+		}
+		unsigned int GetCompact() const
+		{
+			unsigned int nSize=BN_bn2mpi(this, NULL);
+			std::vector<unsigned char> vch(nSize);
+			nSize-=4;
+			BN_bn2mpi(this,&vch[0]);
+			unsigned int nCompact=nSize<<24;
+			if (nSize>=1)nCompact|=(vch[4]<<16);
+			if (nSize>=2) nCompact |=(vch[5]<<8);
+			if (nSize>=3) nCompact |=(vch[6]<<0);
+			return nCompact;
+		}
+		void SetHex (const std::string & str)
+		{
+			const char* psz=str.c_str();
+			while (isspace(*psz))
+				psz++;
+			bool fNegative=false;
+			if (*psz=='-')
+			{
+				fNegative=true;
+				psz++;
+			}
+			if (psz[0]=='0'&&tolower(psz[1])=='x')
+				psz+=2;
+			while (isspace(*psz))
+				psz++;
+			static char phexdigit[256]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,1,2,3,4,5,6,7,8,9,0,0,0,0,0,0, 0,0xa,0xb,0xc,0xd,0xe,0xf,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0xa,0xb,0xc,0xd,0xe,0xf,0,0,0,0,0,0,0,0,0 };
+			*this=0;
+			while (isxdigit(*psz))
+			{
+				*this<<=4;
+				int n=phexdigi[*psz++];
+				*this+=n;
+			}
+			if (fNegative)
+				*this=0-*this;
+		}
+		unsigned int GetSerializeSize(int nType=0, int nVersion=VERSION) const
+		{
+			return::GetSerializeSize(getvch(),nType,nVersion);
+		}
+		template<typename Stream>
+		void Serialize(Stream & s, int nType=0,int nVersion=VERSION) const
+		{
+			::Serialize(s, getvch(),nType,nVersion);
+		}
+		template<typename Stream>
+		void Unserialize(Stream& s, int nType=0, int nVersion=VERSION)
+		{
+			vector<unsigned char> vch;
+			::Unserialize(s, vch,nType,nVersion);
+			setvch(vch);
+		}
+		bool operator!() const
+		{
+			return BN_is_zero(this);
+		}
+		CBigNum& operator +=(const CBigNum&b)
+		{
+			if (!BN_add(this, this, &b))
+				throw bignum_error("CBigNum::operator+=:BN_add failed");
+			return *this;
+		}
+
+		CBigNum& operator -=(const CBigNum&b)
+		{
+			*this=*this-b;
+			return *this;
+		}
+		CBigNum& operator *=(const CBigNum&b)
+		{
+			CAutoBN_CTX pctx;
+			if (!BN_mul(this, this, &b,pctx))
+				throw bignum_error("CBigNum::operator*=:BN_mul failed");
+			return *this;
+		}
+		CBigNum& operator /=(const CBigNum&b)
+		{
+			*this=*this/b;
+			return *this;
+		}
+		CBigNum& operator %=(const CBigNum&b)
+		{
+			*this=*this%b;
+			return *this;
+		}
+		CBigNum& operator <<=(unsigned int shift)
+		{
+			if (!BN_lshift(this, this, shift))
+				throw bignum_error("CBigNum::operator<<=:BN_lshift failed");
+			return *this;
+		}
+		CBigNum& operator >>=(unsigned int shift)
+		{
+			if (!BN_rshift(this, this, shift))
+				throw bignum_error("CBigNum::operator>>=:BN_rshift failed");
+			return *this;
+		}
+		CBigNum& operator++()
+		{
+			if (!BN_add(this, this, BN_value_one()))
+				throw bignum_error("CBigNum::operator++ :BN_add failed");
+			return *this;
+		}
+		const CBigNum operator++ (int)
+		{
+			const CBigNum ret=*this;
+			++ (*this);
+			return *ret;
+		}
+		CBigNum& operator --()
+		{
+			cBigNum r;
+			if (!BN_sub(&r,this, BN_value_one()))
+				throw bignum_error("CBigNum::operator--:BN_sub failed");
+			*this=r;
+			return *this;
+		}
+		const CBigNum operator --(int)
+		{
+			const CBigNum ret=*this;
+			--(*this);
+			return ret;
+		}
+		friend inline const CBigNum operator-(const CBigNum &a, const CBigNum& b);
+		friend inline const CBigNum operator/(const CBigNum& a, const CBigNum& b);
+		friend inline const CBigNum operator%(const CBigNum& a, const CBigNum& b);
+};
+inline const CBigNum operator+(const CBigNum& a, const CBigNum& b)
+{
+	CBigNum r;
+	if (!BN_add(&r, &a, &b))
+		throw bignum_error("CBigNum::operator+:BN_add failed");
+	return r;
+}
+inline const CBigNum operator-(const CBigNum& a, const CBigNum& b)
+{
+	CBigNum r;
+	if (!BN_sub(&r, &a, &b))
+		throw bignum_error("CBigNum::operator- : BN_sub failed");
+	return r;
+}
+inline const CBigNum operator-(const CBigNum& a)
+{
+	CBigNum r(a);
+	BN_set_negative(&r, !BN_is_negative(&r));
+	return r;
+}
+inline const CBigNum operator*(const CBigNum& a, const CBigNum& b)
+{
+	CAutoBN_CTX pctx;
+	CBigNum r;
+	if (!BN_mul(&r, &a, &b,pctx))
+		throw bignum_error("CBigNum::operator* : BN_mul failed");
+	return r;
+}
+inline const CBigNum operator/(const CBigNum& a, const CBigNum& b)
+{
+	CAutoBN_CTX pctx;
+	CBigNum r;
+	if (!BN_div(&r,NULL, &a, &b, pctx))
+		throw bignum_error("CBigNum::operator/ : BN_div failed");
+	return r;
+}
+inline const CBigNum operator%(const CBigNum& a, const CBigNum& b)
+{
+	CAutoBN_CTX pctx;
+	CBigNum r;
+	if (!BN_mod(&r, &a, &b, pctx))
+		throw bignum_error("CBigNum::operator% : BN_mod failed"); //在Bitcoin 0.1.5中,这里有一个bug, BN_mod,写成了BN_div
+	return r;
+}
+inline const CBigNum operator<<(const CBigNum& a, unsigned int shift)
+{
+	CBigNum r;
+	if (!BN_lshift(&r, &a, shift))
+		throw bignum_error("CBigNum::operator<< : BN_lshift failed");
+	return r;
+}
+inline const CBigNum operator>>(const CBigNum& a, unsigned int shift)
+{
+	CBigNum r;
+	if (!BN_rshift(&r, &a, shift))
+		throw bignum_error("CBigNum::operator>> : BN_rshift failed");
+	return r;
+}
+inline bool operator==(const CBigNum& a, const CBigNum& b) {return (BN_cmp(&a, &b)==0);}
+inline bool operator!=(const CBigNum& a, const CBigNum& b) {return (BN_cmp(&a, &b)!=0);}
+inline bool operator<=(const CBigNum& a, const CBigNum& b) {return (BN_cmp(&a, &b)<=0);}
+inline bool operator>=(const CBigNum& a, const CBigNum& b) {return (BN_cmp(&a, &b)>=0);}
+inline bool operator<(const CBigNum& a, const CBigNum& b) {return (BN_cmp(&a, &b)<0);}
+inline bool operator>(const CBigNum& a, const CBigNum& b) {return (BN_cmp(&a, &b)>0);}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
