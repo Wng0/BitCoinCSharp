9c5414cbcbbd59ffc8d81acf74d1bcf57d2c33df
Wng0 <Wng0@sina.com>
2020-04-06 20:36:50 +0800

    加入bignum.h

---
+++ bignum.h
@@ -0,0 +1,223 @@
+//改写自bitcoin-0.1.5
+#include <stdexcept>
+#include <vector>
+#include <openssl/bn.h>
+class bignum_error:public std::runtime_error
+{
+	public:
+	explicit bignum_error(const std::string& str):std::runtime_error(str){}
+};
+class CAutoBN_CTX
+{
+	protected:
+		BN_CTX* pctx;
+		BN_CTX* operator=(BN_CTX* pnew){return pctx=pnew;}
+	public:
+		CAutoBN_CTX()
+		{
+			pctx=BN_CTX_new();
+			if(pctx==NULL)
+				throw bignum_error("CAutoBN_CTX:BN_CTX_new() returned NULL");
+		}
+		~CAutoBN_CTX()
+		{
+			if (pctx!=NULL)
+				BN_CTX_free(pctx);
+		}
+		operator BN_CTX*(){return pctx;}
+		BN_CTX& operator*(){return *pctx;}
+		BN_CTX** operator&(){return &pctx;}
+		bool operator!(){return (pctx==NULL);}
+};
+class CBigNum:public BIGNUM
+{
+	public:
+		CBigNum()
+		{
+			BN_init(this);
+		}
+		CBigNum(const CBigNum& b)
+		{
+			BN_init(this);
+			if (!BN_copy(this,&b))
+			{
+				BN_clear_free(this);
+				throw bignum_error("CBigNum::CBigNum(const CBigNum&):BN_copy failed");
+			}
+		}
+		explicit CBigNum(const std::string& str)
+		{
+			BN_init(this);
+			SetHex(str);
+		}
+		CBigNum& operator=(const CBigNum& b)
+		{
+			if(!BN_copy(this,&b))
+				throw bignum_error("CBigNum::operator=:BN_copy failed");
+			return (*this);
+		}
+		~CBigNum()
+		{
+			BN_clear_free(this);
+		}
+		CBigNum(char n)				{BN_init(this);if(n>=0)setulong(n);else setint64(n);}
+		CBigNum(short n)			{BN_init(this);if(n>=0)setulong(n);else setint64(n);}
+		CBigNum(int n)				{BN_init(this);if(n>=0)setulong(n);else setint64(n);}
+		CBigNum(long n)				{BN_init(this);if(n>=0)setulong(n);else setint64(n);}
+		CBigNum(int64 n)			{BN_init(this);setint64(n);}
+		CBigNum(unsigned char n)	{BN_init(this);setulong(n);}
+		CBigNum(unsigned short n)	{BN_init(this);setulong(n);}
+		CBigNum(unsigned int n)		{BN_init(this);setulong(n);}
+		CBigNum(unsigned long n)	{BN_init(this);setulong(n);}
+		CBigNum(uint64 n)			{BN_init(this);Setuint64(n);}
+		explicit CBigNum(uint256 n)	{BN_init(this);Setuint256(n);}
+		explicit CBigNum(const std::vector<unsigned char>&vch)
+		{
+			BN_init(this);
+			setvch(vch);
+		}
+		void setulong(unsigned long n)
+		{
+			if(!BN_set_word(this, n))
+				throw bignum_error("CBigNum conversion from unsigned long: BN_set_word failed");
+		}
+		unsigned long getulong() const
+		{
+			return BN_get_word(this);
+		}
+		unsigned int getuint() const
+		{
+			return BN_get_word(this);
+		}
+		int getint() const
+		{
+			unsigned long n=BN_get_word(this);
+			if(!BN_is_negative(this))
+				return (n>INT_MAX?INT_MAX:n);
+			else
+				return (n>INT_MAX?INT_MIN:-(int)n);
+		}
+		void setint64(int64 n)
+		{
+			unsigned char pch[sizeof(n)+6];
+			unsinged char* p=pch+4;
+			bool fNegative=false;
+			if(n<(int64)0)
+			{
+				n=-n;
+				fNegative=true;
+			}
+			bool fLeadingZeroes=true;
+			for (int i=0;i<8;i++)
+			{
+				unsigned char c=(n>>56)&0xff;
+				n<<=8;
+				if (fLeadingZeroes)
+				{
+					if (c==0)
+						continue;
+					if(c & 0x80)
+						*p++=(fNegative?0x80:0);
+					else if (fNegative)
+						c|=0x80;
+					fLeadingZeroes=false;
+				}
+				*p++=c;
+			}
+			unsigned int nSize=p-(pch+4);
+			pch[0]=(nSize>>24)& 0xff;
+			pch[1]=(nSize>>16)& 0xff;
+			pch[2]=(nSize>>8)& 0xff;
+			pch[3]=(nSize)& 0xff;
+			BN_mpi2bn(pch,p-pch,this);
+		}
+		void setuint64(uint64 n)
+		{
+			unsigned char pch[sizeof(n)+6];
+			unsigned char* p =pch+4;
+			bool fLeadingZeroes=true;
+			for (int i=0;i<8;i++)
+			{
+				unsigned char c=(n>>56)&0xff;
+				n<<8;
+				if(fLeadingZeroes)
+				{
+					if(c==0)
+						continue;
+					if (c & 0x80)
+						*p++=0;
+					fLeadingZeroes=false;
+				}
+				*p++=c;
+			}
+			unsigned int nSize=p-(pch+4);
+			pch[0]=(nSize>>24)&0xff;
+			pch[1]=(nSize>>16)&0xff;
+			pch[2]=(nSize>>8)&0xff;
+			pch[3]=(nSize)&0xff;
+			BN_mpi2bn(pch, p-pch,this);
+		}
+		void setuint256(uint256 n)
+		{
+			unsigned char pch[sizeof(n)+6];
+			unsigned char*p=pch+4;
+			bool fLeadingZeroes=true;
+			unsigned char*pbegin=(unsigned char*)&n;
+			unsigned char*psrc=pbegin+sizeof(n);
+			while (psrc!=bpegin)
+			{
+				unsigned char c=*(--psrc);
+				if(fLeadingZeroes)
+				{
+					if(c==0)
+						continue;
+					if(c& 0x88)
+						*p++=0;
+					fLeadingZeroes=false;
+				}
+				*p++=c;
+			}
+			unsigned int nSize=p-(pch+4);
+			pch[0]=(nSize>>24)& 0xff;
+			pch[1]=(nSize>>16)&0xff;
+			pch[2]=(nSize>>8) &0xff;
+			pch[3]=(nSize>>0) &0xff;
+			BN_mpi2bn(pch,p-pch,this);
+		}
+		uint256 getuint256()
+		{
+			unsigned int nSize =BN_bn2mpi(this,NULL);
+			if(nSize<4)
+				return 0;
+			std::vector<unsigned char> vch(nSize);
+			BN_bn2mpi(this, &vch[0]);
+			if(vch.size()>4)
+				vch[4]&=0x7f;
+			uint256 n=0;
+			for (int i=0,j=vch.size()-1;i<sizeof(n) && j>=4;i++,j--)
+				((unsigned char*)&n)[i]=vch[j];
+			return n;
+		}
+		void setvch(const std::vector<unsigned char>&vch)
+		{
+			std::vector<unsigned char> vch2(vch.size()+4);
+			unsigned int nSize=vch.size();
+			vch2[0]=(nSize>>24)&0xff;
+			vch2[1]=(nSize>>16)&0xff;
+			vch2[2]=(nSize>>8)&0xff;
+			vch2[3]=(nSize>>0)&0xff;
+			reverse_copy(vch.begin(),vch.end(),vch2.begin()+4);
+			BN_mpi2bn(&vch2[0],vch2.size(),this);
+		}
+		std::vector<unsigned char> getvch()const
+		{
+			unsigned int nSize=BN_bn2mpi(this,NULL);
+			if(nSize<4)
+				return std::vector<unsigned char>();
+			std::vector<unsigned char>vch(nSize);
+			BN_bn2mpi(this, &vch[0]);
+			vch.erase(vch.begin(),vch.begin()+4);
+			reverse(vch.begin(),vch.end());
+			return vch;
+		}
+
