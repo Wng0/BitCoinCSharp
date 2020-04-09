b5eeed2ca7213113a4c551ed276f9ed301e500da
Wng0 <Wng0@sina.com>
2020-04-09 21:20:18 +0800

    完成了db.h    部分

--- db.h
+++ db.h
@@ -208,5 +208,112 @@ public:
 	bool WriteHashBestChain(uint256 hashBestChain);
 	bool LoadBlockIndex();
 };
-
-
+class CReviewDB: public CDB
+{
+public:
+	CReviewDB(const char* pszMode="r+",bool fTxn=false):CDB("reviews.dat",pszMode,fTxn){}
+private:
+	CReviewDB(const CReviewDB&);
+	void operator=(const CReviewDB&);
+public: 
+	bool ReadUser(uint256 hash, CUser& user)
+	{
+		return Read(make_pair(string("user"),hash), user);
+	}
+	bool WriteUser(uint256 hash, const CUser& user)
+	{
+		return Write(make_pair(string("user"),hash),user);
+	}
+	bool ReadReviews(uint256 hash, vector<CReview>& vReviews);
+	bool WriteReviews(uint256 hash, const vector<CReview>& vReviews);
+};
+class CMarketDB: public CDB
+{
+public:
+	CMarketDB(const char* pszMode="r+",bool fTxn=false):CDB("market.dat",pszMode,fTxn){}
+private:
+	CMarketDB(const CMarketDB&);
+	void operator=(const CMarketDB&);
+};
+class CAddrDB:public CDB
+{
+public:
+	CAddrDB(const char* pszMode="r+",bool fTxn=false):CDB("addr.dat",pszMode,fTxn){}
+private:
+	CAddrDB(const CAddrDB&);
+	void operator=(const CAddrDB&);
+public:
+	bool WriteAddress(const CAddress& addr);
+	bool LoadAddresses();
+};
+bool LoadAddresses();
+class CwalletDB:public CDB
+{
+public:
+	CWalletDB(const char* pszMode="r+", bool fTxn=false):CDB("wallet.dat",pszMode,fTxn){}
+private:
+	CWalletDB(const CWalletDB&);
+	void operator=(const CWalletDB&);
+public:
+	bool ReadName(const string& strAddress, string& strName)
+	{
+		strName="";
+		return Read(make_pair(string ("name"), strAddress),strName);
+	}
+	bool WriteName(const string& strAddress,const string& strName)
+	{
+		madAddressBook[strAddress]=strName;
+		return Write(make_pair(string ("name"), strAddress),strName);
+	}
+	bool EraseName(const string& strAddress)
+	{
+		mapAddressBook.erase(strAddress);
+		return Erase(make_pair(string ("name"), strAddress));
+	}
+	bool ReadTx(uint256 hash, CWalletTx& wtx)
+	{
+		return Read(make_pair(string ("tx"),hash),wtx);
+	}
+	bool WriteTx(uint256 hash,const CWalletTx& wtx)
+	{
+		return Write(make_pair(string ("tx"), hash),wtx);
+	}
+	bool EraseTx(uint256 hash)
+	{
+		return Erase(make_pair(string ("tx"),hash));
+	}
+	bool ReadKey(const vector<unsigned char>& vchPubKey, CPrivKey& vchPrivKey)
+	{
+		vchPrivKey.clear();
+		return Read(make_pair(string ("key"), vchPubKey),vchPrivKey);
+	}
+	bool WriteKey(const vector<unsigned char>& vchPubKey, const CPrivKey& vchPrivKey)
+	{
+		return Write(make_pair(string ("key"), vchPubKey), vchPrivKey, false);
+	}
+	bool ReadDefaultKey(vector<unsigned char>& vchPubKey)
+	{
+		vchPubKey.clear();
+		return Read(string("defaultkey"),vchPubKey);
+	}
+	bool WriteDefaultKey(const vector<unsigned char>& vchPubKey)
+	{
+		return Write(string("defaultkey"),vchPubKey);
+	}
+	template<typename T>
+	bool ReadSetting(const string& strkey, T& value) 
+	{
+		return Read(make_pair(string("setting"), strKey), value);
+	}
+	template <typename T>
+	bool WriteSetting(const string& strKey, const T& value)
+	{
+		return Write (make_pair(string("setting"), strKey), value);
+	}
+	bool LoadWallet(vector<unsigned char>& vchDefaultKeyRet);
+};
+bool LoadWallet();
+inline bool SetAddressBookName(const string& strAddress, const string& strName)
+{
+	return CWalletDB().WriteName(strAddress, strName);
+}
