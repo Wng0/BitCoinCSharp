707755f91ece56c6e63bc3076bc82a7dbf11d37b
Wng0 <wng0@sina.com>
2020-06-02 18:56:30 +0800

    更新util.h

--- util.h
+++ util.h
@@ -97,3 +97,63 @@ public:
 #define TRY_CRITICAL_BLOCK(cs)\
     for (bool fcriticalblockonce=true; fcriticalblockonce; assert(("break caught by TRY_CRITICAL_BLOCK!", !fcriticalblockone)),fcriticalblockone=false)\
     for (CTryCriticalBlock criticalblock(cs); fcriticalblockonce && (fcriticalblockonce=criticalblock.Entered()&&(cs.pszFile=__FILE__, cs.nLine=__LINE__,true); fcriticalblockonce=false, cs.pszFile = NULL, cs.nLine=0)
+inline string i64tostr(int64 n)
+{
+    return strprintf("%"PRID64,n);    
+}
+inline string itostr(int n)
+{
+    return strprintf("%d",n);
+}
+inline int64 atoi64(const char* psz)
+{
+#ifdef _MSV_VER
+    return _atoi64(psz);
+#else
+    return strtoll(psz, NULL,10);
+#endif
+}
+inline int64 atoi64(const string& str)
+{
+#ifdef _MSV_VER
+    return _atoi64(str.c_str());
+#else
+    return strtoll(str.c_str(), NULL,10);
+#endif
+}
+inline int atoi(const string& str)
+{
+    return atoi(str.c_str());    
+}
+inline int roundint(double d)
+{
+    return (int)(d>0?d+0.5:d-0.5);
+}
+template<typename T>
+string HexStr(const T itbegin, const T itend, bool fSpaces=true)
+{
+    const unsigned char* pbegin=(const unsigned char*)&itbegin[0];
+    const unsigned char* pend=pbegin+(itend-itbegin)*sizeof(itbegin[0]);
+    string str;
+    for (const unsigned char* p=pbegin; p!=pend;p++)
+        str +=strprintf((fSpaces&& p!=pend-1?"%02x ":"%02x"),*p);
+    return str;
+}
+template<typename T>
+string HexNumStr(const T itbegin, const T itend,bool f0x=true)
+{
+    const unsigned char* pbegin=(const unsigned char*)&itbegin[0];
+    const unsigned char* pend=pbegin+(itend-itbegin)*sizeof(itbegin[0]);
+    string str =(f0x?"0x":"");
+    for (const unsigned char* p=pend-1; p>=pbegin;p--)
+        str+=strprintf("%02X",*p);
+    return str;
+}
+template<typename T>
+void PrintHex(const T pbegin, const T pend, const char* pszFormat="%s", bool fSpace=true)
+{
+    printf(pszFormat, HexStr(pbegin, pend, fSpace).cstr());
+}
+inline int OutputDebugStringF(const char* pszFormat, ...)
+#ifdef __WXDEBUG__
+    FILE* fileout=fopen("debug.log","a");237
