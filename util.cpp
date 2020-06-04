b5929e49d93bdecd356c42e273b37ec952538d83
Wng0 <wng0@sina.com>
2020-06-04 19:44:42 +0800

    完成util.cpp

--- util.cpp
+++ util.cpp
@@ -105,4 +105,167 @@ string strprintf(const char* format,...)
     if(p!=buffer)
         delete p;
     return str;
-} 154
+}
+bool error (const char* format, ...)
+{
+    char buffer[50000];
+    int limit=sizeof(buffer);
+    va_list arg_ptr;
+    va_start(arg_ptr, format);
+    int ret=_vsnprintf(buffer, limit, format, arg_ptr);
+    va_end(arg_ptr);
+    if (ret<0||ret>=limit)
+    {
+        ret=limit-1;
+        buffer[limit-1]=0;        
+    }
+    printf("ERROR:%s\n", buffer);
+    return false;
+}
+void printException(std::exception* pex, const char* pszThread)
+{
+    char pszModule[260];
+    pszModule[0]='\0';
+    GetModuleFileName(NULL, pszModule, sizeof(pszModule));
+    _strlwr(pszModule);
+    char pszMessage[1000];
+    if(pex)
+        snprintf(pszMessage, sizeof(pszMessage), "EXCEPTION: %s       \n%s       \n%s in %s       \n", typeid(*pex).name(),pex->what(),pszModule, pszThread);
+    else
+        snprintf(pszMessage, sizeof(pszMessage), "UNKNOWN EXCEPTION       \n%s in %s       \n",pszModule, pszThread);
+    printf("\n\n************************\n%s",pszMessage);
+    if (wxTheApp)
+        wxMessageBox(pszMessage, "Error",wxOK|wxICON_ERROR);
+    throw;   
+}
+void ParseString(const string& str, char c, vector<string>& v)
+{
+    unsgined int i1=0;
+    unsigned int i2;
+    do
+    {
+        i2=str.find(c,i1);
+        v.push_back(str.substr(i1,i2-i1));
+        i1=i2+1;
+    } while (i2!=str.npos);    
+}
+string FormatMoney(int64 n, bool fPlus)
+{
+    n/=CENT;
+    string str = strprintf("%I64d.%02I64d",(n>0?n:-n)/100,(n>0?n:-n)%100);
+    for (int i=6;i<str.size();i+=4)
+        if(isdigit(str[str.size()-i-1]))
+            str.insert(str.size()-i,1,',');
+    if (n<0)
+        str.insert((unsigned int)0,1,'-');
+    else if (fPlus&&n>0)
+        str.insert((unsigned int)0,1,'+');
+    return str;
+}
+bool ParseMoney(const char* pszIn, int64& nRet)
+{
+    string strWhole;
+    int64 nCents=0;
+    const char* p=pszIn;
+    while(isspace(*p))
+        p++;
+    for (; *p;p++)
+    {
+        if (*p==','&&p>pszIn&&isdigit(p[-1])&&isdigit(p[1])&&isdigit(p[2])&&isdigit(p[3])&&!isdigit(p[4]))
+            continue;
+        if (*p=='.')
+        {
+            p++;
+            if(isdigit(*p))
+            {
+                nCents=10*(*p++-'0');
+                if(isdigit(*p))
+                    nCents+=(*p++-'0');
+            }
+            break;
+        }
+        if (isspace(*p))
+            break;
+        if (!isdigit(*p))
+            return false;
+        strWhole.insert(strWhole.end(),*p);        
+    }
+    for (; *p;p++)
+        if (!isspace(*p))
+            return false;
+    if (strWhole.size()>14)
+        return false;
+    if (nCents<0||nCents>99)
+        return false;
+    int64 nWhole=atoi64(strWhole);
+    int64 nPreValue=nWhole*100+nCents;
+    int64 nValue=nPreValue*CENT;
+    if (nValue/CENT!=nPreValue)
+        return false;
+    if (nValue/COIN !=nWhole)
+        return false;
+    nRet= nValue;
+    return true;   
+}
+bool FileExists(const char* psz)
+{
+#ifdef WIN32
+    return GetFileAttributes(psz)!=-1;
+#else
+    return access(psz,0)!=-1;
+#endif
+}
+int GetFilesize(FILE* file)
+{
+    int nSavePos=ftell(file);
+    int nFilesize=-1;
+    if (fseek(file, 0, SEEK_END)==0)
+        nFilesize=ftell(file);
+    fseek(file,nSavePos, SEEK_SET);
+    return nFilesize;
+}
+uint64 GetRand(uint64 nMax)
+{
+    if (nMax==0)
+        return 0;
+    uint64 nRange=(_UI64_MAX/nMax)*nMax;
+    uint64 nRand=0;
+    do
+        RAND_bytes((unsigned char*)&nRand, sizeof(nRand));
+    while (nRand>=nRange);
+    return (nRand%nMax);    
+}
+int64 GetTime()
+{
+    return time(NULL);
+}
+static int64 nTimeOffset=0;
+int64 GetAdjustedTime()
+{
+    return GetTime()+nTimeOffset;
+}
+void AddTimeData(unsigned int ip, int64 nTime)
+{
+    int64 nOffsetSample=nTime-GetTime();
+    static set<unsigned int> setKnown;
+    if (!setKnown.insert(ip).second)
+        return;
+    static vector<int64> vTimeOffsets;
+    if (vTimeOffsets.empty())
+        vTimeOffsets.push_back(0);
+    vTimeOffsets.push_back(nOffsetSample);
+    printf("Added time data, samples %d,ip %08x, offset %+I64d minutes)\n", vTimeOffsets.size(), ip, vTimeOffsets.back(), vTimeOffsets. back()/60);
+    if (vTimeOffsets.size()>=5&&vTimeOffsets.size()%2==1)
+    {
+        sort(vTimeOffsets.begin(),vTimeOffsets.end());
+        int64 nMedian=vTimeOffsets[vTimeOffsets.size()/2];
+        nTimeOffset=nMedian;
+        if ((nMedian>0?nMedian:-nMedian)>5*60)
+        {
+
+        }
+        foreach (int64 n, vTimeOffsets)
+            printf("%+I64d ",n);
+        printf("| nTimeOffset=%+I64d(% +I64d minutes)\n", nTimeOffset, nTimeOffset/60);
+    }
+}
